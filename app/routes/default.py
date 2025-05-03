35ab55592618f624c4bbae852d13d321de1fb065
LoneHtoo <s_loneway.htoo@ousd.org>
2025-05-02 20:09:47 -0700

    saved my life

--- app/routes/default.py
+++ app/routes/default.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template
 
 # This is for rendering the home page
-@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
@@ -10,6 +10,14 @@ def index():
 # It's a good idea to use the same name/word, and same capitalization conventions
 # Add a route name, function name, and html template name 
 # (then go make that html template)
-@app.route('/newnamehere')
-def newnamehere():
-    return render_template('newnamehere.html')
+@app.route('/projectaboutus')
+def projectaboutus():
+    return render_template('projectaboutus.html')
+
+@app.route('/projectpage')
+def projectpage():
+    return render_template('projectpage.html')
+
+@app.route('/projectquiz')
+def projectquiz():
+    return render_template('projectquiz.html')
