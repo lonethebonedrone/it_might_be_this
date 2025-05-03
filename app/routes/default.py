5caa3a6ef316738ed5b47a96f693af73ae45a1ab
LoneHtoo <s_loneway.htoo@ousd.org>
2025-05-02 21:04:31 -0700

    tony saved my career

--- app/routes/default.py
+++ app/routes/default.py
@@ -2,7 +2,7 @@ from app import app
 from flask import render_template
 
 # This is for rendering the home page
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
 
